a271d08163d18f8f246f528820a6cbc58129fce3
baram55 <asd7973@gmail.com>
2024-08-10 17:34:44 +0900

    [bugFIx #409] 로그인 에러 메세지에 key에 값 할당

--- components/logIn/ErrorMessage.tsx
+++ components/logIn/ErrorMessage.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import S from "@/style/login/login.module.css";
 
 const ErrorMessage = ({ errorMessage }: { errorMessage: string[] }) => {
-  return errorMessage.map((item) => <p className={S.error}>{item}</p>);
+  return errorMessage.map((item) => {
+    const key = crypto.randomUUID();
+
+    return (
+      <p key={key} className={S.error}>
+        {item}
+      </p>
+    );
+  });
 };
 
 export default ErrorMessage;
